95e656cf776ed42e25a02927cfe065269713c46c
Samuel Holgersson <samuel.holgersson@gmail.com>
2022-12-07 12:07:41 +0100

    Day 7 done.

---
+++ day07.py
@@ -0,0 +1,100 @@
+from helper.input import read_input_simple
+import sys
+from collections import defaultdict
+from queue import Queue
+
+
+cache = dict()
+
+
+def folder_size(folder, folder_structure):
+    if folder in cache:
+        return cache[folder]
+    size = 0
+    for a, b in folder_structure[folder]:
+        if a == 'dir':
+            size += folder_size(b, folder_structure)
+        else:
+            size += b
+    cache[folder] = size
+    return size
+
+
+def folder_to_str(folder):
+    return '/'.join(folder)
+
+
+def part1(input):
+    current_folder = []
+    folder_structure = defaultdict(list)
+    all_folders = {''}
+    for line in read_input_simple(7, input):
+        split_line = line.split()
+        if split_line[0] == '$':
+            if split_line[1] == 'ls':
+                continue
+            elif split_line[1] == 'cd':
+                if split_line[2] == '..':
+                    current_folder = current_folder[:-1]
+                elif split_line[2] == '/':
+                    current_folder = ['']
+                else:
+                    current_folder.append(split_line[2])
+        else:
+            new_folder = current_folder + [split_line[1]]
+            if split_line[0] == 'dir':
+                folder_structure[folder_to_str(current_folder)].append(('dir', folder_to_str(new_folder)))
+                all_folders.add(folder_to_str(new_folder))
+            else:
+                folder_structure[folder_to_str(current_folder)].append((folder_to_str(new_folder), int(split_line[0])))
+    small_folders_sum = 0
+    for folder in all_folders:
+        this_folder_size = folder_size(folder, folder_structure)
+        if this_folder_size <= 100000:
+            small_folders_sum += this_folder_size
+    return small_folders_sum
+
+
+def part2(input):
+    current_folder = []
+    folder_structure = defaultdict(list)
+    all_folders = {''}
+    for line in read_input_simple(7, input):
+        split_line = line.split()
+        if split_line[0] == '$':
+            if split_line[1] == 'ls':
+                continue
+            elif split_line[1] == 'cd':
+                if split_line[2] == '..':
+                    current_folder = current_folder[:-1]
+                elif split_line[2] == '/':
+                    current_folder = ['']
+                else:
+                    current_folder.append(split_line[2])
+        else:
+            new_folder = current_folder + [split_line[1]]
+            if split_line[0] == 'dir':
+                folder_structure[folder_to_str(current_folder)].append(('dir', folder_to_str(new_folder)))
+                all_folders.add(folder_to_str(new_folder))
+            else:
+                folder_structure[folder_to_str(current_folder)].append((folder_to_str(new_folder), int(split_line[0])))
+    disk_size = 70000000
+    needed_free_space = 30000000
+    total_used_size = folder_size('', folder_structure)
+    need_to_remove = needed_free_space - disk_size + total_used_size
+    smallest_large_folder_size = disk_size
+    for folder in all_folders:
+        this_folder_size = folder_size(folder, folder_structure)
+        if this_folder_size >= need_to_remove and this_folder_size < smallest_large_folder_size:
+            smallest_large_folder_size = this_folder_size
+    return smallest_large_folder_size
+
+
+if __name__ == '__main__':
+    _, part, input = sys.argv
+    if part == '1':
+        print(part1(input))
+    elif part == '2':
+        print(part2(input))
+    else:
+        print('Part must be one of 1 or 2')
