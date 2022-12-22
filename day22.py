ec4966f0be4bd847c455602b1dfdd1de9f17569d
Samuel Holgersson <samuel.holgersson@gmail.com>
2022-12-22 12:33:10 +0100

    Skeleton for day 22.

---
+++ day22.py
@@ -0,0 +1,20 @@
+from helper.input import read_input
+import sys
+
+
+def part1(input):
+    pass
+
+
+def part2(input):
+    pass
+
+
+if __name__ == '__main__':
+    _, part, input = sys.argv
+    if part == '1':
+        print(part1(input))
+    elif part == '2':
+        print(part2(input))
+    else:
+        print('Part must be one of 1 or 2')
